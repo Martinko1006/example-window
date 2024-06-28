d82a3ae749e6b78f824f75a41c3fac69d11b3c4a
Martin-Vido <vido1@zspionierka.sk>
2024-06-28 12:25:46 +0200

    v0.0

---
+++ examplewindow.py
@@ -0,0 +1,9 @@
+import pygame
+
+window = pygame.display.set_mode((700,500))
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
